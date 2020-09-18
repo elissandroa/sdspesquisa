91ffa7aed174dffd2dd0cf1f1bc3c5a6df8f3f5b
Elissandro <elissandroa@hotmail.com>
2020-09-18 14:31:53 -0300

    Adicionando a página de gráficos

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -4,7 +4,7 @@ import {formatDate} from './helpers';
 import './styles.css';
 import {RecordsResponse} from './types';
 import Pagination from './Pagination';
-import {Link} from 'react-router-dom';
+import Filters from '../../components/Filters';
 const BASE_URL = 'http://localhost:8080'
 
 
@@ -22,14 +22,10 @@ const Records = () => {
     
     return (
         <div className="page-container">
-            <div className="filters-container records-actions">
-                <Link to ="/charts">
-                   <button className="action-filters">
-                       VER GRÁFICOS
-                   </button> 
-                </Link>
-            </div>
-            <table className="records-table" cellPadding="0" cellSpacing="0">
+               <Filters link="/charts" linkText="VER GRÁFICO"  />
+
+               
+               <table className="records-table" cellPadding="0" cellSpacing="0">
                 <thead>
                     <tr>
                         <th>INSTANTE</th>
