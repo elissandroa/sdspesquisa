bc357a41fa744d39f110537501d365c8bf2e600b
Elissandro <elissandroa@hotmail.com>
2020-09-20 21:33:31 -0300

    Configurando front-web para produção

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -5,7 +5,7 @@ import './styles.css';
 import {RecordsResponse} from './types';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters';
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-elissandro.herokuapp.com';
 
 
 const Records = () => {
